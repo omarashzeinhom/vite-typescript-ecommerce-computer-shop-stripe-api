7c4082628ed4e4cf9652a9f7abd2244ea4ba4b4c
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-10-22 20:45:05 +0200

    UPDATED About with Dummy text and container

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,15 @@
+import { MDBContainer } from "mdb-react-ui-kit";
+
 export default function About(){
     return (
-    <div>
+    <MDBContainer >
+      <section>
       <h1>About</h1>
-    </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odit dolorum quaerat unde ex, quo dolor expedita soluta rerum hic dolore qui praesentium cupiditate dolorem rem. Commodi ad distinctio quis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet recusandae obcaecati facere, ex debitis tenetur doloribus provident, maxime aut molestiae atque nostrum consectetur est iste, cum id. Facere, ea odit?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam perferendis magni sit doloremque ex, voluptates rerum ducimus aspernatur facilis laborum eius accusamus quibusdam quia natus quasi! Consectetur voluptas officiis quos.</p>
+      </section>
+   
+    </MDBContainer>
     );
 }
